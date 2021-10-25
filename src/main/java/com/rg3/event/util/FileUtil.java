be100d7153e01983ba4c7a5bd0ae0f8f4da39537
3219005497郑宝柔 <1097641461@qq.com>
2021-10-25 13:31:38 +0800

    [zbr] 实现附加题

--- src/main/java/com/rg3/event/util/FileUtil.java
+++ src/main/java/com/rg3/event/util/FileUtil.java
@@ -1,14 +1,22 @@
 package com.rg3.event.util;
 
+import cn.hutool.core.util.StrUtil;
+import org.junit.jupiter.api.Test;
+
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -85,5 +93,66 @@ public class FileUtil {
             }
         }
 
+    }
+
+    public static List<String> readFile(String filePath)  {
+        ByteArrayOutputStream bos = null;
+        BufferedInputStream bis = null;
+        try {
+             bis = new BufferedInputStream(new FileInputStream(filePath));
+
+            bos = new ByteArrayOutputStream();
+
+            byte[] buffer = new byte[100];
+            int length = 0;
+            while ((length = bis.read(buffer)) != -1){
+                bos.write(buffer, 0, length);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return fileToList(bos.toString());
+//        return null;
+    }
+
+    public static List<String> fileToList(String fileContent){
+        List<String> list = new ArrayList<>();
+        String[] split = fileContent.split("\n");
+
+        for(String content : split){
+            list.add(StrUtil.subBefore(content, "\r", true));
+        }
+        return list;
+    }
+    @Test
+    public void test(){
+//        try {
+//            String res = readFile("1.txt");
+//            List<String> list = new ArrayList<>();
+//            String[] split = res.split("\n");
+//            for (String content : split) {
+//                list.add(StrUtil.subBefore(content,"\r", true));
+//                System.out.println(content);
+//                System.out.println();
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
